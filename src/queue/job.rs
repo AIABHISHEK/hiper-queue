dcbeefde8cb3b958d3fd4f406e6eb41c4efc834d
ABHISHEK <ay22oct02@gmail.com>
2026-01-04 17:50:30 +0530

    intial commit

---
+++ src/queue/job.rs
@@ -0,0 +1,24 @@
+use uuid::Uuid;
+
+pub type JobId = Uuid;
+
+
+pub struct Job {
+    pub id: JobId,
+    pub payload: Vec<u8>,
+    pub retries: u32,
+    pub max_retries: u32,
+}
+
+impl Job {
+    pub fn new(payload: Vec<u8>, max_retries: u8) -> Self {
+        //validation for max_retries
+        //TODO
+        Self {
+            id: Uuid::new_v4(),
+            payload,
+            retries: 0,
+            max_retries,
+        }
+    }
+}
