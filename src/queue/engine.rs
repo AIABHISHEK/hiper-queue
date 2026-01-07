cf5727cb5b8331e39cbd3885ffbacf3ff83a183a
ABHISHEK <ay22oct02@gmail.com>
2026-01-08 00:16:06 +0530

    added command in eng

--- src/queue/engine.rs
+++ src/queue/engine.rs
@@ -1,4 +1,7 @@
 use std::collections::{HashMap, VecDeque};
+use std::time::Instant;
+use tokio::sync::mpsc;
+use tracing::{info, warn};
 
 use super::command::{Command, WorkerId};
 use super::job::{Job, JobId};
@@ -13,4 +16,50 @@ struct InProgressJob {
 pub struct Engine {
     queue: VecDeque<Job>,
     progress_queue: HashMap<JobId, InProgressJob>
+}
+
+impl Engine {
+    pub fn new() -> Self {
+        Self {
+            queue: VecDeque::new(),
+            progress_queue: HashMap::new()
+        }
+    }
+
+    async fn run(&mut self, mut rx: mpsc::Receiver<Command>) {
+        info!("Engine started");
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Command::push(job) => {
+                    info!("Pushed job {}", job.id);
+                    self.queue.push_back(job);
+                }
+                Command::pull(worker_id, reply) => {
+                    let job = self.queue.pop_front();
+
+                    if let Some(job) = job {
+                        let job_id = job.id;
+
+                        self.in_progress.insert(
+                            job_id,
+                            InProgressJob {
+                                job,
+                                worker_id,
+                                started_at: Instant::now(),
+                            },
+                        );
+
+                        let job = self
+                            .in_progress
+                            .get(&job_id)
+                            .map(|entry| entry.job.clone());
+
+                        let _ = reply.send(job);
+                    } else {
+                        let _ = reply.send(None);
+                    }
+                    }
+                }
+            }
+        }
 }
